d4d8690bb5e6fac131eafb4a37009bf6821c4646
Michael Schonfeld <michael@schonfeld.org>
2014-03-07 15:30:33 +0200

    blah

--- 50npm.sh
+++ 50npm.sh
@@ -13,5 +13,5 @@ if [ -d /tmp/deployment/application ]; then
   ln -s /var/node_modules /tmp/deployment/application/
 fi
 
-OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v0.10.21-linux-x64/bin/npm install 2>&1) || error_exit "Failed to run npm install.  $OUT" $?
+OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && sudo /opt/elasticbeanstalk/node-install/node-v0.10.21-linux-x64/bin/npm install 2>&1) || error_exit "Failed to run npm install.  $OUT" $?
 echo $OUT
